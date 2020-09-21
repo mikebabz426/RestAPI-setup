6c740010bd6383f6a07bce6699d23cd2c4e7dd14
mikebabz426 <60781512+mikebabz426@users.noreply.github.com>
2020-09-21 06:35:07 -0500

    added validation

--- index.js
+++ index.js
@@ -26,38 +26,64 @@ app.get("/api/courses", (req, res) => {
 
 app.get("/api/courses/:id", (req, res) => {
 	const course = courses.find((c) => c.id === parseInt(req.params.id));
-	if (!course) res.status(404).send("This course does not exist");
+	if (!course) return res.status(404).send("This course does not exist");
 	res.send(course);
 });
 
 //Add a New Course
 
 app.post("/api/courses", (req, res) => {
-	const schema = Joi.object({
-		name: Joi.string().min(3).required(),
-	});
-
 	//Validates that the new course matches all requirements before adding to database
 
-	const { error, value } = schema.validate(req.body);
-	if (error) {
-		res.status(400).send(error.details[0].message);
-	} else {
-		const course = {
-			id: courses.length + 1,
-			name: value.name,
-		};
-		courses.push(course);
-		res.send(course);
-	}
+	const { error, value } = validateCourse(req.body);
+	if (error) return res.status(400).send(error.details[0].message);
+
+	const course = {
+		id: courses.length + 1,
+		name: value.name,
+	};
+	courses.push(course);
+	res.send(course);
 });
 
 //Updating a Course
 
-app.put("/api/courses/:id", (req, res) => {});
+app.put("/api/courses/:id", (req, res) => {
+	const course = courses.find((c) => c.id === parseInt(req.params.id));
+	if (!course) return res.status(404).send("This course does not exist");
+
+	const { error, value } = validateCourse(req.body);
+	if (error) return res.status(400).send(error.details[0].message);
+
+	course.name = value.name;
+	res.send(course);
+});
+
+//Deleting Courses
+
+app.delete("/api/courses/:id", (req, res) => {
+	const course = courses.find((c) => c.id === parseInt(req.params.id));
+	if (!course) return res.status(404).send("This course does not exist");
+
+	const index = courses.indexOf(course);
+	courses.splice(index, 1);
+
+	res.send(course);
+});
+
+//Server Set-Up
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
 	console.log(`Server listening on port ${PORT}`);
 });
+
+//Utility Functions
+function validateCourse(course) {
+	const schema = Joi.object({
+		name: Joi.string().min(3).required(),
+	});
+
+	return schema.validate(course);
+}
